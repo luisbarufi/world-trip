3cb12032d9e7ce9e218a84cdfafc5014374f1ff4
Luis Barufi <37280785+luisbarufi@users.noreply.github.com>
2022-03-12 23:15:41 -0300

    Update index.tsx

--- src/pages/continents/europe/index.tsx
+++ src/pages/continents/europe/index.tsx
@@ -1,14 +1,19 @@
 import { Flex } from "@chakra-ui/react";
+import { Cities } from "../../../components/Cities";
+import { Content } from "../../../components/Content";
 import ContinentBanner from "../../../components/ContinentBanner/ContinentBanner";
 
 export default function Europe() {
   return (
 
     <Flex w="100%" h="100%" bg="gray.100" direction="column">
-      <ContinentBanner
-        title="Europa"
-        imageName="bannerEurope"
-      />
+      <ContinentBanner title="Europa" imageName="bannerEurope" />
+
+      <Flex  direction="column" maxW="1160px" mx="auto" mb="100" px="1rem">
+        <Content />
+
+        <Cities />
+      </Flex>
     </Flex>
 
   );
